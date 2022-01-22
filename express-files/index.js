9e295cc6ec253dafcd107fbafee01b9bb0d24c9a
Marta Anna Todorovic <matodorovic@Martas-MacBook-Pro.local>
2022-01-23 00:38:57 +0100

    Add express routes for / and /main.css

--- express-files/index.js
+++ express-files/index.js
@@ -1,10 +1,18 @@
 import express, { response } from "express";
+import fs from "fs/promises";
 
 const app = express();
 
-app.get("/", (request, respons) => {
-    response.send("Hello, world!");
+app.get("/", async (request, response) => {
+    const fileBuf = await fs.readFile("./files/index.html");
+    response.type("html");
+    response.send(fileBuf);
 });
 
+app.get("/main.css", async (request, response) => {
+    const fileBuf = await fs.readFile("./files/main.css");
+    response.type("css");
+    response.send(fileBuf);
+});
 
 app.listen(5080);
