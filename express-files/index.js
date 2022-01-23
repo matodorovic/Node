e6880f7f2cc3158a46a0e94564140e473d310357
Marta Anna Todorovic <matodorovic@Martas-MacBook-Pro.local>
2022-01-23 01:06:17 +0100

    Changing directory for css och js files

--- express-files/index.js
+++ express-files/index.js
@@ -15,9 +15,18 @@ app.get("/main.css", async (request, response) => {
     response.send(fileBuf);
 });
 */
-
+/*
 app.get("/:fileName", async (request, response) => {
     const fileName = request.params.fileName;
+    const fileBuf = await fs.readFile(`./files/${fileName}`);
+    const type = fileName.split(".")[1];
+    response.type(type);
+    response.send(fileBuf);
+});
+*/
+
+app.get("/*", async (request, response) => {
+    const fileName = request.path;
     const fileBuf = await fs.readFile(`./files/${fileName}`);
     const type = fileName.split(".")[1];
     response.type(type);
