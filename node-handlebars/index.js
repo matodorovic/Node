07a2eeb59c4c802d38bce92bc61507fd1a1343ab
Marta Anna Todorovic <martaannatodorovic@gmail,com>
2022-02-18 03:24:49 +0100

    Handlebars

---
+++ node-handlebars/index.js
@@ -0,0 +1,44 @@
+import express from "express";
+import fs from "fs/promises";
+import Handlebars from "handlebars";
+
+const app = express();
+
+const menu = [ {
+    label: "Home",
+    link: "/",
+},
+{
+    label: "About us",
+    link: "/about",
+},
+{
+    label: "Contact us",
+    link: "/contact",
+},
+];
+
+app.get("/", async (req, res) => {
+    const htmlBuf = await fs.readFile("./templates/index.handlebars");
+    const htmlText = htmlBuf.toString();
+
+    const template = Handlebars.compile(htmlText);
+    const rendered = template({
+        menu: menu.map(item => {
+            return {
+                link: item.link,
+                label: item.label,
+                active: item.link == "/",
+            };
+        }),
+        path: "/",
+    });
+
+    res.send(rendered);
+});
+
+
+
+app.use("/", express.static("./static"));
+
+app.listen(5080);
